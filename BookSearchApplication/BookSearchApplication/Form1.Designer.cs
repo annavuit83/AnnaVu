5b2cc675ccffbfb5bb04d7b96070346bc3160f39
AnnaVu <annakim2312@gmail.com>
2019-07-25 00:05:17 +0930

    NHon first commit

---
+++ BookSearchApplication/BookSearchApplication/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace BookSearchApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(32, 49);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(277, 365);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(483, 101);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(295, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(483, 160);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(295, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(483, 224);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(295, 22);
+            this.textBox3.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(353, 106);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Author";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(353, 160);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Title";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(353, 224);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "ISBN";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(346, 49);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Search";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(483, 46);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(295, 22);
+            this.textBox4.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(863, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1073, 567);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Book Search";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ BookSearchApplication/BookSearchApplication/Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BookSearchApplication
+{
+    public partial class Form1 : Form
+    {
+        private List<Book> originalBookList;
+        private List<Author> originalAuthorList;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            originalBookList = new List<Book>();
+
+            System.IO.StreamReader file =  new System.IO.StreamReader(@"authors.txt");
+            string line = "";
+            int counter = 0;
+            List<Author> authors = new List<Author>();
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] textLine = line.Split(',');
+                Author author = new Author();
+                author.AuthorID = textLine[0];
+                author.AuthorName = textLine[1];
+
+                authors.Add(author);
+                counter++;
+            }
+
+            originalAuthorList = authors;
+
+            System.IO.StreamReader fileBook = new System.IO.StreamReader(@"Books.txt");
+            string lineBook = "";
+           
+            List<Book> bookList = new List<Book>();
+            while ((lineBook = fileBook.ReadLine()) != null)
+            {
+                string[] textLine = lineBook.Split(',');
+                Book book = new Book();
+                book.BookID = textLine[0];
+                book.AuthorID = textLine[1];
+                book.BookTitle = textLine[2];
+                book.BookISBN = textLine[3];
+
+                bookList.Add(book);
+                
+            }
+
+            originalBookList = bookList;
+
+            for (int i = 0; i < authors.Count; i++)
+            {
+                TreeNode treeNode = new TreeNode();
+                treeNode.Name = authors[i].AuthorName;
+                treeNode.Text = authors[i].AuthorName;
+
+                treeView1.Nodes.Add(treeNode);
+
+                List<Book> booksByAuthorList = getListBookByAuthorID(bookList, authors[i].AuthorID);
+
+                for (int j = 0; j < booksByAuthorList.Count; j++)
+                {
+                    TreeNode bookNode = new TreeNode();
+                    bookNode.Name = booksByAuthorList[j].BookTitle;
+                    bookNode.Text = booksByAuthorList[j].BookTitle;
+
+                    treeNode.Nodes.Add(bookNode);
+                }
+
+
+
+                
+            }
+             
+
+        }
+
+        private List<Book> getListBookByAuthorID(List<Book> originalList, string authorID)
+        {
+            List<Book> returnBooks = new List<Book>();
+
+            for (int i = 0; i < originalList.Count; i++)
+            {
+                if (originalList[i].AuthorID == authorID)
+                {
+                    returnBooks.Add(originalList[i]);
+                }
+            }
+
+            return returnBooks;
+
+        }
+
+        public class Author
+        {
+            public string AuthorID { get; set; }
+            public string AuthorName { get; set; }
+        }
+
+        public class Book
+        {
+            public string BookID { get; set; }
+            public string AuthorID { get; set; }
+
+            public string BookTitle { get; set; }
+
+            public string BookISBN { get; set; }
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+
+            if (treeView1.SelectedNode.Level == 1)
+            {
+                String nodeName = treeView1.SelectedNode.Name;
+
+                Book bookInfo = lookForBookByName(originalBookList, nodeName);
+
+                if (bookInfo != null)
+                {
+                    textBox1.Text = treeView1.SelectedNode.Parent.Name;
+                    textBox2.Text = bookInfo.BookTitle;
+                    textBox3.Text = bookInfo.BookISBN;
+
+                    
+                }
+            }
+            
+
+            
+
+
+            
+
+        }
+
+        private Book lookForBookByName(List<Book> books, string bookName)
+        {
+            Book returnBook = null;
+
+            for (int i = 0; i < originalBookList.Count; i++)
+            {
+                if (originalBookList[i].BookTitle == bookName)
+                {
+                    returnBook = originalBookList[i];
+                    break;
+                }
+
+            }
+
+            return returnBook;
+        }
+
+        private Book lookForBookByISBN(List<Book> books, string ISBN)
+        {
+            Book returnBook = null;
+
+            for (int i = 0; i < originalBookList.Count; i++)
+            {
+                if (originalBookList[i].BookISBN == ISBN)
+                {
+                    returnBook = originalBookList[i];
+                    break;
+                }
+
+            }
+
+            return returnBook;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Book returnBook = null;
+            if (textBox4.Text != null && textBox4.Text != "")
+            {
+                returnBook = lookForBookByName(originalBookList, textBox4.Text);
+
+                if (returnBook == null)
+                {
+                    returnBook = lookForBookByISBN(originalBookList, textBox4.Text);
+                }
+
+                if (returnBook == null)
+                {
+                    MessageBox.Show("There is no book match.");
+                    return;
+                }
+                else
+                {
+                    //textBox2.Text = returnBook.BookTitle;
+                    //textBox3.Text = returnBook.BookISBN;
+
+                    for (int i = 0; i < originalAuthorList.Count; i++)
+                    {
+                        if (originalAuthorList[i].AuthorID == returnBook.BookID)
+                        {
+                            //textBox1.Text = originalAuthorList[i].AuthorName;
+
+                            for (int k = 0; k < treeView1.Nodes.Count; k++)
+                            {
+                                if (treeView1.Nodes[k].Text == originalAuthorList[i].AuthorName)
+                                {
+                                    for (int m = 0; m < treeView1.Nodes[k].Nodes.Count; m++)
+                                    {
+                                        if (treeView1.Nodes[k].Nodes[m].Text == returnBook.BookTitle)
+                                        {
+                                            treeView1.SelectedNode = treeView1.Nodes[k].Nodes[m];
+                                            treeView1.Select();
+                                            treeView1.HideSelection = false;
+                                            break;
+                                        }
+                                    }
+
+                                }
+                            }
+
+
+                           
+                        }
+                    }
+                }
+                
+
+            }
+        }
+    }
+}
