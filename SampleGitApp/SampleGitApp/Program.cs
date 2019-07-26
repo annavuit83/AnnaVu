f7f5455a5c9af204727d1c2c1e2634d8629ea92a
AnnaVu <annakim2312@gmail.com>
2019-07-26 21:16:18 +0930

    Innitialize project

---
+++ SampleGitApp/SampleGitApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SampleGitApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Anna");
+        }
+    }
+}
