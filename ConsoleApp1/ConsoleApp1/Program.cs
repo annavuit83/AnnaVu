aa75dae07f083fb7a726874d57de6459a9365383
AnnaVu <annakim2312@gmail.com>
2019-07-26 20:21:35 +0930

    Change to Anna2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Kevin!");
+            Console.WriteLine("Hello Kevin and Anna!");
         }
     }
 }
