5effdf12d764318e274f63d6ea9c98fcb9fcf28d
AnnaVu <annakim2312@gmail.com>
2019-07-28 20:52:28 +0930

    Change to Kevin

--- TestApp3/TestApp3/Form1.cs
+++ TestApp3/TestApp3/Form1.cs
@@ -16,7 +16,7 @@ namespace TestApp3
         {
             InitializeComponent();
             MessageBox.Show("This is a sentence!!!");
-            MessageBox.Show("This is a Anna.");
+            MessageBox.Show("This is a Kevin");
             MessageBox.Show("This is a David.");
         }
     }
