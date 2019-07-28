bdb8453edec58590e3629394acbb781580b2bb93
AnnaVu <annakim2312@gmail.com>
2019-07-28 22:18:32 +0930

    Add Hello Sunday

--- TestApp2/TestApp2/Form1.cs
+++ TestApp2/TestApp2/Form1.cs
@@ -19,6 +19,8 @@ namespace TestApp2
             MessageBox.Show("Hello Lan Huong + David");
             MessageBox.Show("Hello Kevin");
             MessageBox.Show("Hello Anna");
+
+            MessageBox.Show("Hello Sunday");
         }
     }
 }
